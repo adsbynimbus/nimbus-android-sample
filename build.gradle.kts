900cd53affab9690711c1f698d3f5364b18abbc7
Jason Sznol <jason@timehop.com>
2022-04-06 12:22:53 -0400

    Added check build of sample app for pull requests (#5)
    Fixed plugin resolution

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    val androidGradleVersion = "7.1.2"
+    id("com.android.application") version(androidGradleVersion) apply(false)
+    id("com.android.library") version(androidGradleVersion) apply(false)
+    kotlin("android") version("1.6.20") apply(false)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,18 +6,23 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins {
-        val androidGradleVersion = "7.1.2"
-        id("com.android.application") version(androidGradleVersion)
-        id("com.android.library") version(androidGradleVersion)
-        kotlin("android") version("1.6.20")
-    }
 }
 
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        if (System.getenv("GITHUB_ACTIONS")?.toBoolean() == true) {
+            // If running in Github Actions, use Github packages because it's free
+            maven {
+                url = uri("https://maven.pkg.github.com/timehop/nimbus-openrtb")
+                name = "openrtb"
+                credentials(PasswordCredentials::class)
+                content {
+                    includeGroup("com.adsbynimbus.openrtb")
+                }
+            }
+        }
         maven {
             url = uri("https://adsbynimbus-public.s3.amazonaws.com/android/sdks")
             credentials {

e24ce8d3efbf579450f3294a1796cce11dcd0087
Jason Sznol <jason@timehop.com>
2022-05-13 10:47:28 -0400

    Updated Android Gradle Plugin to 7.2.0 Updated Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    val androidGradleVersion = "7.1.2"
+    val androidGradleVersion = "7.2.0"
     id("com.android.application") version(androidGradleVersion) apply(false)
     id("com.android.library") version(androidGradleVersion) apply(false)
-    kotlin("android") version("1.6.20") apply(false)
+    kotlin("android") version("1.6.21") apply(false)
 }

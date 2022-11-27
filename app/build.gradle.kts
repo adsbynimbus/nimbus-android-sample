444b3fa7113bc637ee619ff5e3903d58a17a5aa5
Jason Sznol <jason@timehop.com>
2022-11-27 13:47:57 -0500

    Updated Nimbus Version to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("nimbus.app")
 }
 
-val nimbusVersion = "2.0.0"
+val nimbusVersion = "2.1.1"
 
 /* The compileSdk, minSdk, and targetSdk are applied in the build-logic/src/main/kotlin/nimbus.app.gradle.kts plugin */
 android {

8c8ccdac4fe754705e594e39623737491f201d5d
Renam Antunes <renam@timehop.com>
2023-05-12 16:57:59 -0300

    removes unity id check from AdDemoFragment since it's no longer necessary, unity was moved to different section

--- app/src/main/java/com/adsbynimbus/android/sample/admanager/AdDemoFragment.kt
+++ app/src/main/java/com/adsbynimbus/android/sample/admanager/AdDemoFragment.kt
@@ -7,14 +7,10 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.adsbynimbus.android.sample.BuildConfig
 import com.adsbynimbus.android.sample.R
 import com.adsbynimbus.android.sample.common.SampleAppAdapter
-import com.adsbynimbus.android.sample.common.showCustomDialog
 import com.adsbynimbus.android.sample.databinding.FragmentAdDemoBinding
-import com.amazon.aps.ads.ApsAd
 
 class AdDemoFragment : Fragment() {
 
@@ -28,15 +24,11 @@ class AdDemoFragment : Fragment() {
         headerView.setSubtitleText(resources.getString(R.string.ad_demo_subtitle))
 
         val adItemAdapter = SampleAppAdapter("showAdDemo", enumValues<AdItem>()) { item ->
-            if (BuildConfig.UNITY_GAME_ID.isEmpty()) {
-                showCustomDialog("UNITY_GAME_ID", inflater, root.context).show()
-            } else {
-                findNavController().navigate(R.id.to_adManagerFragment, bundleOf(
-                    "item" to item,
-                    "titleText" to item.description,
-                    "subtitleText" to titleText,
-                ))
-            }
+            findNavController().navigate(R.id.to_adManagerFragment, bundleOf(
+                "item" to item,
+                "titleText" to item.description,
+                "subtitleText" to titleText,
+            ))
         }
 
         recyclerView.adapter = adItemAdapter

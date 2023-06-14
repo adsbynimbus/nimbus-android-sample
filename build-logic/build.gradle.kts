e99742145cb941750e879b9488ebcfcb0d9332c5
Jason Sznol <jason@timehop.com>
2023-06-14 11:55:09 -0400

    Updated Nimbus to 2.10.0

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.0.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
 }

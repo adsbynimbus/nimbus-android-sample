3dfb64a50cf1514a06e912a4eee04c302afc1652
Renam Philippi Antunes <renam@timehop.com>
2024-03-21 14:01:00 -0300

    updates nimbus to 2.19.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("nimbus.app")
 }
 
-val nimbusVersion = "2.18.0"
+val nimbusVersion = "2.19.0"
 
 /* The compileSdk, minSdk, and targetSdk are applied in the build-logic/src/main/kotlin/nimbus.app.gradle.kts plugin */
 android {

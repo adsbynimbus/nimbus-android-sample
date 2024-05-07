8fff528be3bc39bd522be5b47a6529e7a7a63be2
Renam Philippi Antunes <renam@timehop.com>
2024-05-07 12:30:20 -0300

    updates dependencies

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.1.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    implementation("com.android.tools.build:gradle:8.1.4")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
 }

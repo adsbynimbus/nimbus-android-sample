46cee8e568772bf69edabc215c5942316639b930
Renam Philippi Antunes <renam@timehop.com>
2024-10-09 15:51:00 -0300

    update nimbus to 2.23.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("nimbus.app")
 }
 
-val nimbusVersion = "2.22.0"
+val nimbusVersion = "2.23.0"
 
 /* The compileSdk, minSdk, and targetSdk are applied in the build-logic/src/main/kotlin/nimbus.app.gradle.kts plugin */
 android {

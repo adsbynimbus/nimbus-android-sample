f21b56938ac993d71df1480db5385401b73713de
Renam Philippi Antunes <renam@timehop.com>
2024-10-14 11:06:14 -0300

    updates agp and kotlin plugin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.1.4")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    implementation("com.android.tools.build:gradle:8.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.10")
 }

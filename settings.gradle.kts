4d1ead94693631bf8d4f1f8fdcacc90e5f74650f
Renam Philippi Antunes <renam@nimbus.co>
2025-12-04 18:03:19 -0300

    removes references to nimbus-openrtb from gradle file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,16 +26,6 @@ dependencyResolutionManagement {
             }
         }
         mavenCentral()
-        if (providers.environmentVariable("GITHUB_ACTIONS").isPresent) {
-            // If running in Github Actions, use Github packages because it's free
-            maven("https://maven.pkg.github.com/timehop/nimbus-openrtb") {
-                name = "openrtb"
-                credentials(PasswordCredentials::class)
-                content {
-                    includeGroup("com.adsbynimbus.openrtb")
-                }
-            }
-        }
         // Provides access to Nimbus SDK artifacts
         maven("https://adsbynimbus-public.s3.amazonaws.com/android/sdks") {
             content {

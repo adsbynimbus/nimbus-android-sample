eda8c760189fe950c398aa1cd7e0ca223d28417a
Renam Philippi Antunes <renam@nimbus.co>
2026-04-08 19:49:42 -0300

    changes to support 3.0

--- app/src/main/kotlin/rendering/Positioning.kt
+++ app/src/main/kotlin/rendering/Positioning.kt
@@ -3,7 +3,7 @@ package com.adsbynimbus.android.sample.rendering
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
 import androidx.core.view.updateLayoutParams
-import com.adsbynimbus.render.AdController
+import com.adsbynimbus.render.internal.AdController
 
 inline fun AdController.align(alignment: () -> Int) {
     view?.updateLayoutParams<FrameLayout.LayoutParams> {

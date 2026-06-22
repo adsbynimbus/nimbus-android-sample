fd2af00ada713ae58326bea969c1e3bc1c4c5465
Renam Philippi Antunes <renam@nimbus.co>
2026-06-22 11:21:36 -0300

    expands samples for digital turbine

--- app/src/main/kotlin/demand/DigitalTurbine.kt
+++ app/src/main/kotlin/demand/DigitalTurbine.kt
@@ -1,22 +1,28 @@
 package com.adsbynimbus.android.sample.demand
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.view.Gravity.CENTER_HORIZONTAL
 import android.view.Gravity.TOP
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.adsbynimbus.*
+import com.adsbynimbus.android.sample.databinding.LayoutDigitalTurbineNativeAdBinding
 import com.adsbynimbus.android.sample.databinding.LayoutInlineAdBinding
 import com.adsbynimbus.android.sample.rendering.ScreenAdLogger
 import com.adsbynimbus.android.sample.rendering.disableAllExtensions
 import com.adsbynimbus.extension.DigitalTurbineExtension
 import com.adsbynimbus.rtb.Position.Header
+import com.fyber.inneractive.sdk.external.MediaView
+import com.fyber.inneractive.sdk.external.NativeAdContent
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
+
 
 class DigitalTurbineFragment : Fragment() {
 
@@ -30,7 +36,7 @@ class DigitalTurbineFragment : Fragment() {
         disableAllExtensions()
         Nimbus.extensions<DigitalTurbineExtension>()?.enabled = true
         when (val item = requireArguments().getString("item")) {
-            "Banner" -> viewLifecycleOwner.lifecycleScope.launch {
+            "Banner" -> lifecycleScope.launch {
                 val logger = ScreenAdLogger(identifier = item, logView = logs)
                 ads += Nimbus.bannerAd(position = item, size = AdSize.Banner, adPosition = Header)
                     .onEvent {
@@ -44,7 +50,7 @@ class DigitalTurbineFragment : Fragment() {
                         }
                     }
             }
-            "MREC" -> viewLifecycleOwner.lifecycleScope.launch {
+            "MREC" -> lifecycleScope.launch {
                 val logger = ScreenAdLogger(identifier = item, logView = logs)
                 ads += Nimbus.bannerAd(position = item, size = AdSize.Mrec, adPosition = Header)
                     .onEvent {
@@ -58,7 +64,7 @@ class DigitalTurbineFragment : Fragment() {
                         }
                     }
             }
-            "Interstitial" -> viewLifecycleOwner.lifecycleScope.launch {
+            "Interstitial" -> lifecycleScope.launch {
                 val logger = ScreenAdLogger(identifier = item, logView = logs)
                 ads += Nimbus.interstitialAd(position = item) {
                     video()
@@ -68,7 +74,7 @@ class DigitalTurbineFragment : Fragment() {
                     logger.onError(it)
                 }.show(this@DigitalTurbineFragment, closeButtonDelay = 10.seconds)
             }
-            "Rewarded" -> viewLifecycleOwner.lifecycleScope.launch {
+            "Rewarded" -> lifecycleScope.launch {
                 val logger = ScreenAdLogger(identifier = item, logView = logs)
                 ads += Nimbus.rewardedAd(position = item).onEvent {
                     logger.onAdEvent(it)
@@ -76,11 +82,55 @@ class DigitalTurbineFragment : Fragment() {
                     logger.onError(it)
                 }.show(this@DigitalTurbineFragment, closeButtonDelay = 10.seconds)
             }
+            "Native" -> lifecycleScope.launch {
+
+                DigitalTurbineExtension.nativeAdViewProvider = DigitalTurbineExtension.NativeAdViewProvider { container, nativeAd ->
+                    val binding = LayoutDigitalTurbineNativeAdBinding.inflate(LayoutInflater.from(requireContext()))
+                    populateNativeAd(binding, nativeAd)
+                    binding.root
+                }
+
+                val logger = ScreenAdLogger(identifier = item, logView = logs)
+                ads += Nimbus.inlineAd(item) {
+                    native()
+                }.onEvent {
+                    logger.onAdEvent(it)
+                }.onError {
+                    logger.onError(it)
+                }.show(adFrame)
+            }
         }
     }.root
+
+    private fun populateNativeAd(
+        binding: LayoutDigitalTurbineNativeAdBinding,
+        nativeAd: NativeAdContent,
+    ) {
+        val adTitle: String? = nativeAd.adTitle
+        val adDescription: String? = nativeAd.adDescription
+        val appIconUri: Uri? = nativeAd.appIcon
+        val callToAction: String? = nativeAd.adCallToAction
+        val rating: Float? = nativeAd.rating
+        val mediaView: MediaView? = nativeAd.mediaView
+        val mediaAspectRatio: Float? = nativeAd.mediaAspectRatio
+
+        binding.adTitle.text = adTitle
+        binding.adDescription.text = adDescription
+        binding.adIcon.setImageURI(appIconUri)
+        binding.adCtaBtn.text = callToAction
+        rating?.let { binding.adRating.rating }
+        binding.mediaViewContainer.addView(mediaView)
+        binding.mediaViewContainer.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            dimensionRatio = mediaAspectRatio.toString()
+        }
+
+        nativeAd.registerViewsForInteraction(binding.root,
+            nativeAd.mediaView, binding.adIcon, listOf(binding.adCtaBtn, binding.adRating, binding.adDescription, binding.adTitle))
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         ads.forEach { it.destroy() }
+        DigitalTurbineExtension.nativeAdViewProvider = null
     }
 }
